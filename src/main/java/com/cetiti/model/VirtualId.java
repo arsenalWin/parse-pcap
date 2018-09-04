abed04ce1545fe6e82698887ff72c7734fbd193d
zhouwei <stern1991@163.com>
2018-09-04 15:15:28 +0800

    虚拟身份类

---
+++ src/main/java/com/cetiti/model/VirtualId.java
@@ -0,0 +1,35 @@
+package com.cetiti.model;
+
+/**
+ * @Author: zhouwei
+ * @Description: 虚拟身份类
+ * @Date: Create in 10:23 2018-9-4
+ * @Modified By:
+ */
+public class VirtualId {
+  /**
+   *  QQ号
+   */
+  private String qq;
+
+  /**
+   * IMEI信息
+   */
+  private String imei;
+
+  public String getQq() {
+    return qq;
+  }
+
+  public void setQq(String qq) {
+    this.qq = qq;
+  }
+
+  public String getImei() {
+    return imei;
+  }
+
+  public void setImei(String imei) {
+    this.imei = imei;
+  }
+}
