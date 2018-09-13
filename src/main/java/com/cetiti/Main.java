ea8d36ff6733c4aefd01dcafe4c24e650e07d780
zhouwei <stern1991@163.com>
2018-09-13 11:48:13 +0800

    添加日志

--- src/main/java/com/cetiti/Main.java
+++ src/main/java/com/cetiti/Main.java
@@ -3,6 +3,8 @@ package com.cetiti;
 import com.cetiti.handler.TCPPacketHandler;
 import com.cetiti.handler.TCPPacketSearchHandler;
 import io.pkts.Pcap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -13,14 +15,22 @@ import java.io.IOException;
  * @Modified By:
  */
 public class Main {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
   public static void main(final String[] args) throws IOException{
     final Pcap pcap = Pcap.openStream("dump.pcap");
+
+    LOGGER.info("开始解析包");
 
 //    TCPPacketSearchHandler packetHandler = new TCPPacketSearchHandler();
     TCPPacketHandler packetHandler = new TCPPacketHandler();
 
     pcap.loop(packetHandler);
 
+    LOGGER.info("完成包的解析");
+
     packetHandler.dumpToFile("result.yml");
+
+    LOGGER.info("写入结果成功");
   }
 }
